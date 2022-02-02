e794170b888bb685c485c4b863694b08adeeaa6f
xxsddm <1224278193@qq.com>
2022-02-02 23:18:47 +0800

    更新每日习题

--- acwing/1701-1800/1762 牛的洗牌 .cpp
+++ acwing/1701-1800/1762 牛的洗牌 .cpp
@@ -6,15 +6,6 @@ using namespace std;
 
 int N;
 
-void move(int *a, int *nums, int *aux) {
-	for (int i = 1; i <= N; i++) {
-		aux[i] = nums[i];
-	}
-	for (int i = 1; i <= N; i++) {
-		nums[i] = aux[a[i]];
-	}
-}
-
 int main() {
 	scanf("%d", &N);
 	int a[N + 1], nums[N + 1], aux[N + 1];
@@ -22,10 +13,10 @@ int main() {
 		scanf("%d", &a[i]);
 	}
 	for (int i = 1; i <= N; i++) {
-		scanf("%d", &nums[i]);
+		scanf("%d", &aux[i]);
 	}
-	for (int i = 0; i < 3; i++) {
-		move(a, nums, aux);
+	for (int i = 1; i <= N; i++) {
+		nums[i] = aux[a[a[a[i]]]];
 	}
 	for (int i = 1; i <= N; i++) {
 		printf("%d\n", nums[i]);
