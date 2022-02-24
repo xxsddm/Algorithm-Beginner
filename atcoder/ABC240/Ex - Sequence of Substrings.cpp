3e9785dd228fe3ad58f97377525abc68d17a7b1e
xxsddm <1224278193@qq.com>
2022-02-24 12:14:57 +0800

    优化代码, 增加注释

--- atcoder/ABC240/Ex - Sequence of Substrings.cpp
+++ atcoder/ABC240/Ex - Sequence of Substrings.cpp
@@ -16,9 +16,9 @@ struct Node {
 	}
 } *root = new Node();
 
-int N, *counter, *dp;
+int N, *counter;	// counter[idx] -> 1~idx的最大子序列数量(字典序递增)
 
-int query(int idx) {	// 树状数组保存截止idx的最大子序列数量
+int query(int idx) {	// 树状数组
 	int temp = 0;
 	for (int i = idx; i; i ^= i & -i) {
 		temp = max(temp, counter[i]);
@@ -28,21 +28,20 @@ int query(int idx) {	// 树状数组保存截止idx的最大子序列数量
 
 void change(int idx, int num) {
 	for (int i = idx; i <= N; i += i & -i) {
-		counter[i] = max(counter[i], num);
+		if (num <= counter[i]) {
+			break;
+		}
+		counter[i] = num;
 	}
 }
 
-void dfs(Node *node, int length) {	// 前序遍历, 遍历节点字典序递增
+void dfs(Node * node, int length) {	// 前序遍历, 遍历节点字典序递增
 	if (node == nullptr) {
 		return;
 	}
 	for (int i = (int) node->idx.size() - 1; i >= 0; i--) {
 		int &idx = node->idx[i];
-		int temp = query(idx - length) + 1;
-		if (temp > dp[idx]) {
-			dp[idx] = temp;
-			change(idx, temp);
-		}
+		change(idx, query(idx - length) + 1);
 	}
 	dfs(node->next[0], length + 1);
 	dfs(node->next[1], length + 1);
@@ -52,10 +51,9 @@ int main() {	// 树状DP
 	scanf("%d", &N);
 	char container[N + 2];
 	scanf("%s", container + 1);
-	int maxLength = (int) sqrt(2 * N + 0.1), ans = 0;
-	counter = new int[N + 1], dp = new int[N + 1];
+	int maxLength = (int) sqrt(2 * N + 0.1);
+	counter = new int[N + 1];
 	memset(counter, 0, sizeof(int) * (N + 1));
-	memset(dp, 0, sizeof(int) * (N + 1));
 	for (int start = 1; start <= N; start++) {
 		Node *node = root;
 		for (int length = 1, limit = min(N - start + 1, maxLength); length <= limit; length++) {
@@ -68,9 +66,6 @@ int main() {	// 树状DP
 		}
 	}
 	dfs(root, 0);
-	for (int i = 1; i <= N; i++) {
-		ans = max(ans, dp[i]);
-	}
-	printf("%d", ans);
+	printf("%d", query(N));
 	return 0;
 }
