9e3ccf5a48ef4925596807749b8eda5b8c12084c
xxsddm <1224278193@qq.com>
2022-03-15 22:50:14 +0800

    更新每日一题

---
+++ acwing/3301-3400/3346 你知道你的ABC吗 .cpp
@@ -0,0 +1,16 @@
+// https://www.acwing.com/problem/content/3349/
+
+#include "algorithm"
+#include "iostream"
+
+using namespace std;
+
+int main() {
+	int nums[7];
+	for (int i = 0; i < 7; i++) {
+		scanf("%d", &nums[i]);
+	}
+	sort(nums, nums + 7);
+	printf("%d %d %d", nums[0], nums[1], nums[6] - nums[0] - nums[1]);
+	return 0;
+}
