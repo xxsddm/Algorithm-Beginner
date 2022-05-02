36fc86c712733d541b4fd788531084a5a24822be
xxsddm <1224278193@qq.com>
2022-05-02 14:17:52 +0800

    更新Tarjan离线LCA

--- libreoj/137 最小瓶颈路 加强版.cpp
+++ libreoj/137 最小瓶颈路 加强版.cpp
@@ -134,3 +134,123 @@ int main() {
 	write(ans);
 	return 0;
 }
+
+/*
+#include "algorithm"
+#include "vector"
+#include "iostream"
+
+template <typename T>
+inline void read(T &x) {
+	x = 0;
+	char c = getchar();
+	while (c < 48 || c > 57) {
+		c = getchar();
+	}
+	while (c >= 48 && c <= 57) {
+		x = (x << 3) + (x << 1) + (c ^ 48);
+		c = getchar();
+	}
+}
+
+int numstk[40];
+
+inline void write(int x) {
+	int length = 0;
+	while (x > 9) {
+		numstk[length++] = x % 10;
+		x /= 10;
+	}
+	numstk[length++] = x % 10;
+	for (int i = length - 1; i >= 0; i--) {
+		putchar(numstk[i] ^ 48);
+	}
+}
+
+struct Edge {
+	int u, v, w;
+
+	bool operator < (Edge &other) {
+		return w < other.w;
+	}
+} *edges;
+
+int n, m, idx = 1, ans = 0, mod = 1e9 + 7, a, b, c, p;
+int *root, *val, **next;
+std::vector<int> *query;
+
+inline int rnd() {
+	return a = (a * b % p + c) % p;
+}
+
+int find(int node) {
+	while (node != root[node]) {
+		root[node] = root[root[node]];
+		node = root[node];
+	}
+	return node;
+}
+
+void addNode(int node1, int node2, int weight) {
+	int idx1 = find(node1), idx2 = find(node2);
+	if (idx1 == idx2) {
+		return;
+	}
+	root[idx1] = root[idx2] = idx + n;
+	next[idx] = new int[2];
+	next[idx][0] = idx1, next[idx][1] = idx2;
+	val[idx++] = weight;
+}
+
+void dfs(int node) {
+	root[node] = node;
+	if (node <= n) {
+		for (int &other : query[node]) {
+			if (root[other] != -1) {
+				ans = (ans + val[find(other) - n]) % mod;
+			}
+		}
+		return;
+	}
+	for (int i = 0; i < 2; i++) {
+		int &nextNode = next[node - n][i];
+		dfs(nextNode);
+		root[nextNode] = node;
+	}
+}
+
+int main() {
+	read(n), read(m);
+	edges = new Edge[m];
+	for (int i = 0; i < m; i++) {
+		read(edges[i].u), read(edges[i].v), read(edges[i].w);
+	}
+	int length = n << 1;
+	root = new int[length], val = new int[n];
+	next = new int*[n];
+	query = new std::vector<int>[n + 1];
+	std::sort(edges, edges + m);
+	for (int i = 1; i < length; i++) {
+		root[i] = i;
+	}
+	for (int i = 0; i < m && idx < n; i++) {
+		addNode(edges[i].u, edges[i].v, edges[i].w);
+	}
+	int q;
+	read(q), read(a), read(b), read(c), read(p);
+	for (int i = 0, u, v; i < q; i++) {
+		u = rnd() % n + 1, v = rnd() % n + 1;
+		if (u != v) {
+			query[u].push_back(v);
+			query[v].push_back(u);
+		}
+	}
+	for (int i = 1; i < length; i++) {
+		root[i] = -1;
+	}
+	dfs(length - 1);
+	write(ans);
+	return 0;
+}
+
+ */
